fce8099d84ccede46251a2d27f272ea6e52ce9cf
alpal <>
2022-11-10 11:58:56 +0000

    Small tweaks to file paths

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import os
 import random
 import pandas as pd
 
-adjectives = pd.read_csv(os.path.join(os.path.dirname(__file__), 'words/adjectives.txt'), sep='\n', header=None, names=['adjectives'], squeeze=True)
-animals    = pd.read_csv(os.path.join(os.path.dirname(__file__), 'words/animals.txt'),    sep='\n', header=None, names=['animals'],    squeeze=True)
+adjectives = pd.read_csv(os.path.join(os.path.dirname(__file__), 'words', 'adjectives.txt'), sep='\n', header=None, names=['adjectives'], squeeze=True)
+animals    = pd.read_csv(os.path.join(os.path.dirname(__file__), 'words', 'animals.txt'),    sep='\n', header=None, names=['animals'],    squeeze=True)
 
 print((random.choice(adjectives) + random.choice(animals)).lower().replace('-', '').strip())
